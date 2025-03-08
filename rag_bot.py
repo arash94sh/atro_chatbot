474af5cd937b90ce9e8bea0435fb64f11ec518d9
arash94sh <88932490+arash94sh@users.noreply.github.com>
2025-03-08 14:12:24 +0330

    rag_bot fix

--- rag_bot.py
+++ rag_bot.py
@@ -44,32 +44,35 @@ vector_store = Chroma(
 
 
 # #rpompt crafting
-# prompt = ChatPromptTemplate(input_variables=['context', 'question'], input_types={}, partial_variables={}, metadata={'lc_hub_owner': 'rlm', 'lc_hub_repo': 'rag-prompt', 'lc_hub_commit_hash': '50442af133e61576e74536c6556cefe1fac147cad032f4377b60c436e6cdcb6e'}, messages=[HumanMessagePromptTemplate(prompt=PromptTemplate(input_variables=['context', 'question'], input_types={}, partial_variables={}, template="you are an assistant that answers the users question regarding the services a company provide based on the content of this website, this is the persian website and the users are persians so you must speak in farsi.when there is no context find reqarding the question say that  you don't knowledge regarding the question.\nQuestion: {question} \nContext: {context} \nAnswer:"), additional_kwargs={})])
+prompt = ChatPromptTemplate(input_variables=['context', 'question'], input_types={}, partial_variables={}, metadata={'lc_hub_owner': 'rlm', 'lc_hub_repo': 'rag-prompt', 'lc_hub_commit_hash': '50442af133e61576e74536c6556cefe1fac147cad032f4377b60c436e6cdcb6e'}, messages=[HumanMessagePromptTemplate(prompt=PromptTemplate(input_variables=['context', 'question'], input_types={}, partial_variables={}, template="you are an assistant that answers the users question regarding the services a company provide based on the content of this website, this is the persian website and the users are persians so you must speak in farsi.when there is no context find reqarding the question say that  you don't knowledge regarding the question.\nQuestion: {question} \nContext: {context} \nAnswer:"), additional_kwargs={})])
 
-# # Define state for application
-# class State(TypedDict):
-#     question: str
-#     context: List[Document]
-#     answer: str
-
-
-# # Define application steps
-# def retrieve(state: State):
-#     retrieved_docs = vector_store.similarity_search(state["question"])
-#     return {"context": retrieved_docs}
+# Define state for application
+class State(TypedDict):
+    question: str
+    context: List[Document]
+    answer: str
 
 
-# def generate(state: State):
-#     docs_content = "\n\n".join(doc.page_content for doc in state["context"])
-#     messages = prompt.invoke({"question": state["question"], "context": docs_content})
-#     response = llm.invoke(messages)
-#     return {"answer": response.content}
+# Define application steps
+def retrieve(state: State):
+    retrieved_docs = vector_store.similarity_search(state["question"])
+    return {"context": retrieved_docs}
 
 
-# # Compile application and test
-# graph_builder = StateGraph(State).add_sequence([retrieve, generate])
-# graph_builder.add_edge(START, "retrieve")
-# graph = graph_builder.compile()
+def generate(state: State):
+    docs_content = "\n\n".join(doc.page_content for doc in state["context"])
+    messages = prompt.invoke({"question": state["question"], "context": docs_content})
+    response = llm.invoke(messages)
+    return {"answer": response.content}
+
+
+# Compile application and test
+graph_builder = StateGraph(State).add_sequence([retrieve, generate])
+graph_builder.add_edge(START, "retrieve")
+graph = graph_builder.compile()
+
+
+
 
 
 
@@ -100,90 +103,3 @@ if input_text := st.chat_input():
 
 
 
-graph_builder = StateGraph(MessagesState)
-
-
-
-@tool(response_format="content_and_artifact")
-def retrieve(query: str):
-    """Retrieve information related to a query."""
-    retrieved_docs = vector_store.similarity_search(query, k=2)
-    serialized = "\n\n".join(
-        (f"Source: {doc.metadata}\n" f"Content: {doc.page_content}")
-        for doc in retrieved_docs
-    )
-    return serialized, retrieved_docs
-
-
-
-    from langchain_core.messages import SystemMessage
-from langgraph.prebuilt import ToolNode
-
-
-# Step 1: Generate an AIMessage that may include a tool-call to be sent.
-def query_or_respond(state: MessagesState):
-    """Generate tool call for retrieval or respond."""
-    llm_with_tools = llm.bind_tools([retrieve])
-    response = llm_with_tools.invoke(state["messages"])
-    # MessagesState appends messages to state instead of overwriting
-    return {"messages": [response]}
-
-
-# Step 2: Execute the retrieval.
-tools = ToolNode([retrieve])
-
-
-# Step 3: Generate a response using the retrieved content.
-def generate(state: MessagesState):
-    """Generate answer."""
-    # Get generated ToolMessages
-    recent_tool_messages = []
-    for message in reversed(state["messages"]):
-        if message.type == "tool":
-            recent_tool_messages.append(message)
-        else:
-            break
-    tool_messages = recent_tool_messages[::-1]
-
-    # Format into prompt
-    docs_content = "\n\n".join(doc.content for doc in tool_messages)
-    system_message_content = (
-        "You are an assistant for question-answering tasks. "
-        "Use the following pieces of retrieved context to answer "
-        "the question. If you don't know the answer, say that you "
-        "don't know. Use three sentences maximum and keep the "
-        "answer concise."
-        "your users speak in farsi so you must answer them in farsi"
-        "you shouls not answer questions unrelated to the vectore database contnets"
-        "\n\n"
-        f"{docs_content}"
-    )
-    conversation_messages = [
-        message
-        for message in state["messages"]
-        if message.type in ("human", "system")
-        or (message.type == "ai" and not message.tool_calls)
-    ]
-    prompt = [SystemMessage(system_message_content)] + conversation_messages
-
-    # Run
-    response = llm.invoke(prompt)
-    return {"messages": [response]}
-
-
-
-
-graph_builder.add_node(query_or_respond)
-graph_builder.add_node(tools)
-graph_builder.add_node(generate)
-
-graph_builder.set_entry_point("query_or_respond")
-graph_builder.add_conditional_edges(
-    "query_or_respond",
-    tools_condition,
-    {END: END, "tools": "tools"},
-)
-graph_builder.add_edge("tools", "generate")
-graph_builder.add_edge("generate", END)
-
-graph = graph_builder.compile()
